aa5584da8da560e47da9441989e4ab8779531ca6
madhurpv <madhurpreetamvaidya@gmail.com>
2025-03-11 19:48:29 +0530

    Added BirthdayListActivity.java

--- app/src/main/java/com/example/licpolicyhelper/BirthdayListActivity.java
+++ app/src/main/java/com/example/licpolicyhelper/BirthdayListActivity.java
@@ -1,42 +1,24 @@
 package com.example.licpolicyhelper;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class BirthdaySettingsActivity extends AppCompatActivity {
-
-
-    CardView birthdayListCard;
+public class BirthdayListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_birthday_settings);
+        setContentView(R.layout.activity_birthday_list);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-
-        birthdayListCard = findViewById(R.id.birthdayListCard);
-
-
-        birthdayListCard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent myIntent = new Intent(BirthdaySettingsActivity.this, BirthdayListActivity.class);
-                BirthdaySettingsActivity.this.startActivity(myIntent);
-            }
         });
     }
 }
