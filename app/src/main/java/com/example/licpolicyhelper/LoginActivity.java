bef1accd7e62260383be31b604aa4ae14817baa3
madhurpv <madhurpreetamvaidya@gmail.com>
2025-03-20 11:31:53 +0530

    Added Forgot Password Dialog at LoginActivity.java

--- app/src/main/java/com/example/licpolicyhelper/LoginActivity.java
+++ app/src/main/java/com/example/licpolicyhelper/LoginActivity.java
@@ -1,10 +1,14 @@
 package com.example.licpolicyhelper;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +20,7 @@ public class LoginActivity extends AppCompatActivity {
 
     EditText usernameEditText, passwordEditText;
     Button submitButton;
+    TextView forgotPasswordTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +37,7 @@ public class LoginActivity extends AppCompatActivity {
         usernameEditText = findViewById(R.id.usernameEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
         submitButton = findViewById(R.id.submitButton);
+        forgotPasswordTextView = findViewById(R.id.forgotPasswordTextView);
 
 
         submitButton.setOnClickListener(new View.OnClickListener() {
@@ -43,7 +49,41 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        forgotPasswordTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Intent myIntent = new Intent(LoginActivity.this, MainActivity.class);
+                LoginActivity.this.startActivity(myIntent);
+                finish();*/
+                showMessage1PopUpDialog();
+            }
+        });
 
 
+
+    }
+
+
+
+    private void showMessage1PopUpDialog() {
+        // Create a new dialog
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.login_forgotpassword_popupdialog);
+        //dialog.setCancelable(true); // Allows dismissing by tapping outside
+        dialog.setCancelable(false);
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+        // Get references to views in the dialog
+        TextView title = dialog.findViewById(R.id.dialogTitle);
+        Button okButton = dialog.findViewById(R.id.okButton);
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss(); // Close the dialog
+            }
+        });
+
+        dialog.show();
     }
 }
